6d1ad2e59a89170fc39660d33e85b399737551c2
AntonioNeto98 <141379093+AntonioNeto98@users.noreply.github.com>
2024-01-23 17:55:07 -0300

    add

--- oo_js/main.js
+++ oo_js/main.js
@@ -7,4 +7,8 @@ function Moto (modelo, cilindrada, fabricante, anoModelo, anoFabricacao) {
 }
 
 const motoDoJonata = new Moto ("cb300", "300", "Honda", 2012, 2012 );
-const motoDaAline = new Moto ("Superleggera V4", "998", "Ducati", 2020, 2020); 
+const motoDaAline = new Moto ("Superleggera V4", "998", "Ducati", 2020, 2020);
+
+console.log (motoDoJonata instanceof Moto);
+console.log (motoDaAline instanceof Moto);
+console.log (motoDoJonata instanceof Array);
