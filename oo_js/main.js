daba92908396489e2a481cb3bdf903dc2f16e524
AntonioNeto98 <141379093+AntonioNeto98@users.noreply.github.com>
2024-01-23 17:53:00 -0300

    first commit

---
+++ oo_js/main.js
@@ -0,0 +1,10 @@
+function Moto (modelo, cilindrada, fabricante, anoModelo, anoFabricacao) {
+    this.modelo = modelo;
+    this.cilindrada = cilindrada;
+    this.fabricante = fabricante;
+    this.anoModelo = anoModelo;
+    this.anoFabricacao = anoFabricacao;
+}
+
+const motoDoJonata = new Moto ("cb300", "300", "Honda", 2012, 2012 );
+const motoDaAline = new Moto ("Superleggera V4", "998", "Ducati", 2020, 2020); 
